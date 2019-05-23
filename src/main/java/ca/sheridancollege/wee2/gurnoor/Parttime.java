9f3381f0a681a0d0645d36fb6a49672f9361f83c
Karan Bath <Karan Bath@10.48.42.168>
2019-05-23 13:00:07 -0400

    second commit

---
+++ src/main/java/ca/sheridancollege/wee2/gurnoor/Parttime.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.wee2.gurnoor;
+
+/**
+ *
+ * @author Karan Bath
+ */
+public class Parttime {
+    private int hellllllllllo;
+
+    /**
+     * @return the hellllllllllo
+     */
+    public int getHellllllllllo() {
+        return hellllllllllo;
+    }
+
+    /**
+     * @param hellllllllllo the hellllllllllo to set
+     */
+    public void setHellllllllllo(int hellllllllllo) {
+        this.hellllllllllo = hellllllllllo;
+    }
+    
+}
